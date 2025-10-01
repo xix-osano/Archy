1edf9df570bf2c2dd9d6a68e0241292eaa0d792f
xix-osano <osanoenosh@gmail.com>
2025-10-01 21:44:05 +0300

    first commit

---
+++ install/login/all.sh
@@ -0,0 +1,4 @@
+run_logged $ARCHY_INSTALL/login/plymouth.sh
+run_logged $ARCHY_INSTALL/login/limine-snapper.sh
+run_logged $ARCHY_INSTALL/login/enable-mkinitcpio.sh
+run_logged $ARCHY_INSTALL/login/alt-bootloaders.sh
