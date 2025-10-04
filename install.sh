d1caba29a6aa369b2a53aba15ec6e2fa94b32a4c
xix-osano <osanoenosh@gmail.com>
2025-10-04 14:42:16 +0300

    Updated structure

--- install.sh
+++ install.sh
@@ -1,5 +1,30 @@
 #!/bin/bash
 
+# Set install mode to online since boot.sh is used for curl installations
+export ARCHY_ONLINE_INSTALL=true
+
+ansi_art='                 ▄▄▄                                                   
+   ▄███████   ▄███████   ▄███████   ▄█   █▄    ▄█   █▄ 
+  ███   ███  ███   ███  ███   ███  ███   ███  ███   ███
+  ███   ███  ███   ███  ███   █▀   ███   ███  ███   ███
+ ▄███▄▄▄███ ▄███▄▄▄██▀  ███       ▄███▄▄▄███▄ ███▄▄▄███
+ ▀███▀▀▀███ ▀███▀▀▀▀    ███      ▀▀███▀▀▀███  ▀▀▀▀▀▀███
+  ███   ███ ██████████  ███   █▄   ███   ███  ▄██   ███
+  ███   ███  ███   ███  ███   ███  ███   ███  ███   ███
+  ███   █▀   ███   ███  ███████▀   ███   █▀    ▀█████▀ 
+             ███   █▀                                  '
+
+clear
+echo -e "\n$ansi_art\n"
+
+sudo pacman -Syu --noconfirm --needed git
+
+echo -e "\nCloning Archy from: https://github.com/xix-osano/Archy.git"
+rm -rf ~/.local/share/archy/
+git clone "https://github.com/xix-osano/Archy.git" ~/.local/share/archy >/dev/null
+
+echo -e "\nInstallation starting..."
+
 # Exit immediately if a command exits with a non-zero status
 set -eEo pipefail
 
